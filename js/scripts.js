66f9189b1a2894f7ae78886a890cbeaeb038c9a2
Sophie <sophielamb3@outlook.com>
2019-05-14 17:33:51 +0100

    Updates scripts page with Pokemon information

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-alert ('Hello World! Welcome to my page!');
+var repositary = [
+  var pokemon {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass' , 'poison']
+  }
+  var pokemon {
+
+  }
+];
