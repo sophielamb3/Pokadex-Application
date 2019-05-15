2df9ba0718ec960521ca95745790a9476b93bf10
Sophie <sophielamb3@outlook.com>
2019-05-15 17:36:43 +0100

    updated repositary in scripts.js page

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 // This array will become the repository of Pokémon to display in your application
 
-var repositary = [
-  { name: 'Bulbasaur',
-    height: 2.4,
-    type: ['grass' , 'poison']
-  },
-  { name: 'Charmander',
-    height: 5.7,
-    type: ['fire' , 'flying']
-  },
-  { name: 'Squirtle',
-    height: 3.7,
-    type: ['water' , 'physchic']
-  },
+var repository = [
+    {
+        name: 'Bulbasaur',
+        height: 2.4,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charizard',
+        height: 5.7,
+        types: ['fire', 'flying']
+    },
+    {
+        name: 'Butterfree',
+        height: 3.7,
+        types: ['bug', 'flying']
+    },
+
 ];
 
 for (var i = 0; i < repositary.length; i++){
