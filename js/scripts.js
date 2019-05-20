75059f831c7b71b10ab017aeb62c0f51dd1c8392
Sophie <sophielamb3@outlook.com>
2019-05-20 19:38:32 +0100

    Updated code to include IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,36 @@
 // This array will become the repository of Pokémon to display in your application
+var pokemonRepository = (function () {
+  var repository = [
+      {
+          name: 'Bulbasaur',
+          height: 2.4,
+          types: ['grass', 'poison']
+      },
+      {
+          name: 'Charizard',
+          height: 5.7,
+          types: ['fire', 'flying']
+      },
+      {
+          name: 'Butterfree',
+          height: 3.7,
+          types: ['bug', 'flying']
+      },
+    ];
 
-var repository = [
-    {
-        name: 'Bulbasaur',
-        height: 2.4,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Charizard',
-        height: 5.7,
-        types: ['fire', 'flying']
-    },
-    {
-        name: 'Butterfree',
-        height: 3.7,
-        types: ['bug', 'flying']
-    },
+  function add(pokemon){
+    repository.push(pokemon);
+  }
 
-];
+  function getAll() {
+    return repository;
+  }
 
-//for (var i = 0; i < repository.length; i++){
-//  if (repository[i].height > 4){
-//    document.write(repository[i].name + '\'s height is: ' + repository[i].height + ' feet.'  + "WOW! Thats big!" + '<br>' + "My powers are:" + repository[i].types + '<br>');
-//  } else {
-//    document.write(repository[i].name + '\'s height is: ' + repository[i].height + ' feet.' + '<br>'  + "My powers are:" + repository[i].types + '<br>');
-//  }
-// }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 
 Object.keys(repository).forEach(function(property) {
