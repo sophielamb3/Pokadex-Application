660370089c7b665072c15f1666546f19fc08b89f
Sophie <sophielamb3@outlook.com>
2019-05-25 14:05:47 +0100

    Added addListItem() and showDetails(). Added event listener too

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,11 @@ var pokemonRepository = (function () {
           height: 3.7,
           types: ['bug', 'flying']
       },
+      {
+          name: 'Pikachu',
+          height: 1.5,
+          types: ['yellow', 'cute']
+      },
     ];
 
   function add(pokemon){
@@ -26,17 +31,39 @@ var pokemonRepository = (function () {
     return repository;
   }
 
+  function addListItem(pokemonObject){
+    var $newListItem = document.createElement('li');
+    var $newButton = document.createElement('button');
+    var $div = document.querySelector('div');
+
+    $newListItem.setAttribute('class','grid');
+    $newButton.setAttribute('class', 'button');
+    $div.appendChild($newListItem);
+    $div.appendChild($newButton);
+    $newButton.innerText = pokemonObject.name;
+    $newButton.addEventListener ('click',
+      function (event) {
+        console.log(event.target.innerText);
+    });
+
+  }
+
+  function showDetails(pokemon){
+    console.log(pokemonObject)
+  }
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
 var allPokemons = pokemonRepository.getAll()
 console.log(allPokemons)
 allPokemons.forEach(function (pokemon) {
-  return ((pokemon.name + ': ' + '<br>' + pokemon.height + '<br>' + pokemon.types + '<br>'))
-})
+  return (pokemonRepository.addListItem(pokemon)
+)})
 
 // NOTE code below wouldn't work before as i was trying to define a local variable rather than global!!! //
 
